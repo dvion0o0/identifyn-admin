37ee5a3daab430cb321ff2db78fb20ad4adc7ae4
digvijay39 <digvijayghosh39@gmail.com>
2023-12-20 13:19:18 +0530

    mobile support text fix

--- src/pages/NotSupported/index.js
+++ src/pages/NotSupported/index.js
@@ -7,7 +7,7 @@ const Index = () => {
         <div className='container' style={{display:'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop:'60px', height:'100vh'}}>
         <i class="fas fa-cloud" aria-hidden="true" style={{ fontSize: '100px', color: 'var(--primary-color)', marginBottom:'20px'}}></i>
        <h1 style={{marginBottom: '20px', fontSize:'30px'}}>Oops!</h1>
-      <div style={{marginBottom: '20px', fontSize:'30px', color:'var(--primary-color)', fontWeight:'500'}} >Mobile Version is not Supported.</div>
+      <div style={{marginBottom: '20px', fontSize:'30px', color:'var(--primary-color)', fontWeight:'500', textAlign:'center'}} >Mobile Version is not Supported.</div>
       <div>
       </div>
       </div>
